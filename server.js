2e5b7dd99470e79ea430eeb40e871c144ab4b8ac
repl.it user <replituser@example.com>
2020-12-21 09:22:52 +0000

    commit after succesfully finishing the challenges and also editing the html and css files.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+let bodyParser = require('body-parser')
+let multer = require('multer')
+let upload = multer()
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,19 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('The App is listening on port : '+ port)
+})
